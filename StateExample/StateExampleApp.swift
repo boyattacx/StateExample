1ae50eb02bbbb61de1b672ae52148f43dfbd5eb7
Boy van Amstel <boy@boyvanamstel.nl>
2023-02-08 20:56:07 +0100

    Initial Commit

---
+++ StateExample/StateExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateExampleApp.swift
+//  StateExample
+//
+//  Created by Boy van Amstel on 08/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct StateExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
